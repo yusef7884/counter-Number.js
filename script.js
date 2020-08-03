dbfe266486377fde5c62e1915029756d00fcc217
yusef7884 <64807950+yusef7884@users.noreply.github.com>
2020-08-03 01:54:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ let counter = document.querySelector('.counter');
+ const addCount = document.querySelector('#addCountBtn');
+ const lowerCount = document.querySelector('#lowerCountBtn');
+
+ let count = 0;
+
+ addCount.addEventListener('click' ,incrementCounter);
+ lowerCount.addEventListener('click',decrementCounter);
+
+ function incrementCounter () {
+     count++;
+     counter.innerHTML = count;
+
+     if(counter.innerHTML>'0'){
+         counter.style.color = 'green'
+     }else if(counter.innerHTML === '0'){
+         counter.style.color = 'white';
+     }
+     counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:800,fill:'forwards'});
+ }
+
+ function decrementCounter () {
+    count--;
+    counter.innerHTML = count;
+
+    if(counter.innerHTML<'0'){
+        counter.style.color = 'red'
+    }else if(counter.innerHTML === '0'){
+        counter.style.color = 'white';
+    }
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'});
+}
